1abad764e7ed78db1a74dc40550fb3763394358d
fvilers <d0f4Jld38uPFBpiMSL5J>
2014-03-17 13:31:49 +0100

    Added project with the AsyncPSCmdlet

---
+++ AsyncPowerShell/AsyncPowerShell/AsyncPSCmdlet.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Threading;
+using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace AsyncPowerShell
+{
+    public class AsyncPSCmdlet : PSCmdlet
+    {
+        protected virtual Task ProcessRecordAsync()
+        {
+            return Task.Delay(0);
+        }
+
+        protected sealed override void ProcessRecord()
+        {
+            AsyncPump.Run(async delegate
+            {
+                await ProcessRecordAsync();
+            });
+        }
+    }
+}
